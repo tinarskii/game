dc313afa15c3a6a3650c7948f82a1c7ba5da0e8b
tinarskii <tinvv@outlook.co.th>
2026-04-14 22:45:48 +0700

    refactor: declare playBtnRec for readability and increase Title font size

--- src/SceneManager.cpp
+++ src/SceneManager.cpp
@@ -7,10 +7,17 @@
 int SceneManager::PlayTitleScene(Scene *scene)
 {
     const char *text = "Title Screen";
-    const int fontSize = 48;
+    const int fontSize = 128;
     int textWidth = MeasureText(text, fontSize);
     DrawText("Title Screen", (GetScreenWidth() / 2) - textWidth / 2, (GetScreenHeight() / 6) - fontSize / 6, fontSize, LIGHTGRAY);
-    if(GuiButton((Rectangle){GetScreenWidth() / 2, GetScreenHeight()/2, 200, 30}, "Play")) {
+
+    float rectW = 300.0f,
+        rectH = 50.0f,
+        rectX = ((float)GetScreenWidth()/2) - rectW/2,
+        rectY = ((float)GetScreenHeight()/2) - rectH/2;
+    Rectangle playBtnRec = {rectX, rectY, rectW, rectH};
+    if (GuiButton(playBtnRec, "Play Game!"))
+    {
         *scene = Scene::Game;
     }
     return 0;
